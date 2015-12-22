2163fe996e4d8a8e60032bf040f9a16af8a78ac5
Daniel Chang <daniel.chang85@gmail.com>
2015-12-21 22:42:15 -0500

    simplify game serializer

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,11 +1,5 @@
 class GameSerializer < ActiveModel::Serializer
   attributes :id, :human, :bot, :winner_id
-
-  def human
-    PlayerSerializer.new(object.human).serializable_object if object.human.present?
-  end
-
-  def bot
-    PlayerSerializer.new(object.bot).serializable_object
-  end
+  has_one :human, serializer: PlayerSerializer
+  has_one :bot, serializer: PlayerSerializer
 end
