ff08e450064f11ed07255d013e14d03e934ce77c
Daniel Chang <daniel.chang85@gmail.com>
2015-12-27 21:16:37 -0500

    Add validation for number of ships on a board

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -6,4 +6,13 @@ class Ship < ActiveRecord::Base
   validates_uniqueness_of :row, scope: [:column, :board_id]
   validates_inclusion_of :row, :in => Board::SIZE
   validates_inclusion_of :column, :in => Board::SIZE
+  validate :under_ship_limit
+
+  private
+
+  def under_ship_limit
+    if board.ships.count >= Board::SHIPS_ALLOWED
+      errors.add :board_id, "Board already has enough ships."
+    end
+  end
 end
--- spec/models/ship_spec.rb
+++ spec/models/ship_spec.rb
@@ -2,6 +2,31 @@ require 'rails_helper'
 
 describe Ship do
   describe 'validates ship coordinates are within board' do
+    context 'board already has max ships' do
+      let!(:board) { create(:board) }
+      let(:extra_ship) { build(:ship, board_id: board.id, row: 4, column: 4) }
+
+      before do # create ten ships
+        create(:ship, board_id: board.id, row: 1, column: 1)
+        create(:ship, board_id: board.id, row: 1, column: 2)
+        create(:ship, board_id: board.id, row: 1, column: 3)
+        create(:ship, board_id: board.id, row: 1, column: 4)
+        create(:ship, board_id: board.id, row: 1, column: 5)
+
+        create(:ship, board_id: board.id, row: 2, column: 1)
+        create(:ship, board_id: board.id, row: 3, column: 1)
+        create(:ship, board_id: board.id, row: 4, column: 1)
+        create(:ship, board_id: board.id, row: 5, column: 1)
+        create(:ship, board_id: board.id, row: 5, column: 5)
+      end
+
+      it 'does not allow any extra ships to be created' do
+        extra_ship.save
+        expect(extra_ship.errors).not_to be_empty
+        expect(extra_ship.errors.first).to include(/Board already has enough ships./)
+      end
+    end
+
     context 'coordinate outside board' do
       let!(:invalid_ship) { build(:ship, :invalid_coordinate) }
 
