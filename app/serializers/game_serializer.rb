e0124238fd9c6164ba25a67f4fad265e36a4c7c9
Daniel Chang <daniel.chang85@gmail.com>
2016-01-02 16:27:38 -0500

    Fix inflector. Modify game serializer

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,6 +1,4 @@
 class GameSerializer < ActiveModel::Serializer
   attributes :id, :winner_id, :created_at, :updated_at
-
-  has_one :human, polymorphic: true
-  has_one :bot, polymorphic: true
+  has_many :players, polymorphic: true, serializer: PlayerSerializer
 end
